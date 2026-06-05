eadacca84ceabae7a5ae90c26d4703ea7bbdd4c4
Rui-Yang Ju <102137894+RuiyangJu@users.noreply.github.com>
2026-06-05 16:35:02 +0900

    Update ocr.py

--- ndlkotenocr-lite/ocr.py
+++ ndlkotenocr-lite/ocr.py
@@ -143,9 +143,8 @@ def process(args):
         print("Images are not found.")
         return
 
-    if not os.path.exists(args.output):
-        print("Output Directory is not found.")
-        return
+    os.makedirs(args.output, exist_ok=True)
+    print(f"[INFO] Output directory: {args.output}")
 
     print(inputpathlist)
 
@@ -380,7 +379,7 @@ if __name__ == "__main__":
     parser.add_argument("--det-iou-threshold", type=float, required=False, default=0.3)
     parser.add_argument("--rec-weights", type=str, required=False, default="model/parseq-ndl-32x384-tiny-10.onnx")
     parser.add_argument("--rec-classes", type=str, required=False, default="config/NDLmoji.yaml")
-    parser.add_argument("--device", type=str, required=False, choices=["cpu", "cuda"], default="cpu")
+    parser.add_argument("--device", type=str, required=False, choices=["cpu", "cuda"], default="gpu")
     parser.add_argument("--gpu-id", type=int, required=False, default=0)
 
     args = parser.parse_args()
