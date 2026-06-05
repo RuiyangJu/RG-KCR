6b290020b9a284a8862e7a35f555e8384aa6a75a
Rui-Yang Ju <102137894+RuiyangJu@users.noreply.github.com>
2026-06-05 18:05:11 +0900

    Update ops.py

--- yolov12/ultralytics/utils/ops.py
+++ yolov12/ultralytics/utils/ops.py
@@ -174,7 +174,7 @@ def non_max_suppression(
     labels=(),
     max_det=300,
     nc=0,  # number of classes (optional)
-    max_time_img=0.05,
+    max_time_img=10000,
     max_nms=30000,
     max_wh=7680,
     in_place=True,
