039013021438293d384239c8dd3aa28c87a45b0f
Rui-Yang Ju <102137894+RuiyangJu@users.noreply.github.com>
2026-06-06 18:30:15 +0900

    Add files via upload

---
+++ detection/predict.py
@@ -0,0 +1,28 @@
+import argparse
+from pathlib import Path
+from ultralytics import YOLO, RTDETR
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--model", required=True)
+parser.add_argument("--source", required=True)
+
+args = parser.parse_args()
+
+model_name = Path(args.model).stem
+
+if "rtdetr" in args.model.lower():
+    model = RTDETR(args.model)
+else:
+    model = YOLO(args.model)
+
+model.predict(
+    source=args.source,
+    conf=0.1,
+    save=False,
+    save_txt=True,
+    save_conf=True,
+    project="./detection/runs/detect",
+    name=f"test_{model_name}",
+    exist_ok=True
+)
